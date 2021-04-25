56f05dd8db6471453b25dbd5d4bf442d3a8002f5
ziggy <frmax@student.chalmers.se>
2021-04-25 15:37:13 +0200

    Add initial screen face finding support

---
+++ screen_face_finder.py
@@ -0,0 +1,34 @@
+from time import sleep
+
+from PIL import ImageGrab
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+
+
+def process_img(image):
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
+
+
+def main():
+    while True:
+        screen = np.array(ImageGrab.grab(bbox=(0, 40, 2560, 1440)))
+        faces = face_cascade.detectMultiScale(screen, 1.35, 5)
+
+        for (x, y, w, h) in faces:
+
+            cv2.rectangle(screen, (x, y), (x + w, y + h), (255, 255, 0), 2)
+
+        cv2.imshow('img', screen)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+        sleep(0.5)
+
+
+main()
