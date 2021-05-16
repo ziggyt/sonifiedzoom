05874dec629f3de2d8f5bf4c2bbc627f8cad947e
ziggy <frmax@student.chalmers.se>
2021-05-16 21:07:06 +0200

    Add rate of change speed thingy

--- face2midi-v4.py
+++ face2midi-v4.py
@@ -47,7 +47,7 @@ def x_coordinate_to_midi(coordinate):
 #
 #     return mapped_value
 
-def translate_x_to_midi(value): #https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+def translate_x_to_midi(value):  # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
     # Figure out how 'wide' each range is
     leftSpan = X_MAX - X_MIN
     rightSpan = MIDI_MAX - MIDI_MIN
@@ -59,7 +59,7 @@ def translate_x_to_midi(value): #https://stackoverflow.com/questions/1969240/map
     return int(MIDI_MIN + (valueScaled * rightSpan))
 
 
-def translate_y_to_midi(value): #https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+def translate_y_to_midi(value):  # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
     # Figure out how 'wide' each range is
     leftSpan = Y_MAX - Y_MIN
     rightSpan = MIDI_MAX - MIDI_MIN
@@ -70,7 +70,21 @@ def translate_y_to_midi(value): #https://stackoverflow.com/questions/1969240/map
     # Convert the 0-1 range into a value in the right range.
     return int(MIDI_MIN + (valueScaled * rightSpan))
 
+
+def translate_diff_to_midi(value):  # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+    # Figure out how 'wide' each range is
+    leftSpan = X_MAX - X_MIN
+    rightSpan = MIDI_MAX - MIDI_MIN
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - X_MIN) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(MIDI_MIN + (valueScaled * rightSpan))
+
+
 def main():
+    old_face = Face(0, 0, 0, 0)
     while True:
         screen = np.array(ImageGrab.grab(bbox=(X_MIN, Y_MIN, X_MAX, Y_MAX)))
         faces = face_cascade.detectMultiScale(screen, 1.35, 5)
@@ -84,10 +98,23 @@ def main():
             x_note = translate_x_to_midi(face.x)
             y_note = translate_y_to_midi(face.y)
 
+            if old_face.x != 0:
+                diff_val = abs(face.x - old_face.x)
+
+                try:
+                    diff_note = translate_diff_to_midi(diff_val ** 1.2)
+
+                    vcv_midi_device.send_midi_velocity_note_to_channel(int(diff_note), channel=2)
+                    print(f'SENT VALUE {diff_note} TO CHANNEL 2')
+                except:
+                    print('Something went wrong when parsing movement rate')
+
             vcv_midi_device.send_midi_velocity_note_to_channel(x_note, channel=0)
             print(f'SENT VALUE {x_note} TO CHANNEL 0')
             vcv_midi_device.send_midi_velocity_note_to_channel(y_note, channel=1)
             print(f'SENT VALUE {y_note} TO CHANNEL 1')
+
+            old_face = face
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
