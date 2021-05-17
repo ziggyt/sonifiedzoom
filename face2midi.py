2e7f724c6b98af47be7edf5216c99ba71cb2ac48
ziggy <frmax@student.chalmers.se>
2021-05-17 10:20:04 +0200

    Cleanup

--- face2midi.py
+++ face2midi.py
@@ -4,26 +4,21 @@ from PIL import ImageGrab
 import cv2
 import numpy as np
 
+from face import Face
+from midi_device import MidiDevice
+
 face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
 
-Y_MIN = 0
-Y_MAX = 1440
+Y_MIN = 200
+Y_MAX = 1000
 
-X_MIN = 0
-X_MAX = 2560
+X_MIN = 450
+X_MAX = 2100
 
 MIDI_MIN = 40
 MIDI_MAX = 90
 
-virtual_midi_out = mido.open_output('Sonified Zoom', virtual=True)
-
-
-def send_midi_note(note: int, state: bool = True):
-    midi_state = 'note_on' if state else 'note_off'
-
-    msg = mido.Message(midi_state, note=note, velocity = note)
-
-    virtual_midi_out.send(msg)
+vcv_midi_device = MidiDevice()
 
 
 def x_coordinate_to_midi(coordinate):
@@ -35,35 +30,105 @@ def x_coordinate_to_midi(coordinate):
     return mapped_value
 
 
-def turn_off_all_notes():
-    for i in range(127):
-        send_midi_note(i, False)
+# def y_coordinate_to_midi(coordinate):
+#     value = float(coordinate) / float(Y_MAX)
+#
+#     mapped_value = int(value * MIDI_MAX)
+#     print(mapped_value)
+#
+#     return mapped_value
+#
+#
+# def x_coordinate_to_midi(coordinate):
+#     value = float(coordinate) / float(X_MAX)
+#
+#     mapped_value = int(value * MIDI_MAX)
+#     print(mapped_value)
+#
+#     return mapped_value
+
+def translate_x_to_midi(value):  # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+    # Figure out how 'wide' each range is
+    leftSpan = X_MAX - X_MIN
+    rightSpan = MIDI_MAX - MIDI_MIN
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - X_MIN) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(MIDI_MIN + (valueScaled * rightSpan))
+
+
+def translate_y_to_midi(value):  # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+    # Figure out how 'wide' each range is
+    leftSpan = Y_MAX - Y_MIN
+    rightSpan = MIDI_MAX - MIDI_MIN
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - Y_MIN) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(MIDI_MIN + (valueScaled * rightSpan))
+
+
+def translate_diff_to_midi(value):  # https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+    # Figure out how 'wide' each range is
+    leftSpan = X_MAX - X_MIN
+    rightSpan = MIDI_MAX - MIDI_MIN
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - X_MIN) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(MIDI_MIN + (valueScaled * rightSpan))
 
 
 def main():
+    old_face = Face(0, 0, 0, 0)
     while True:
         screen = np.array(ImageGrab.grab(bbox=(X_MIN, Y_MIN, X_MAX, Y_MAX)))
         faces = face_cascade.detectMultiScale(screen, 1.35, 5)
 
         if len(faces) > 0:
 
-            face = faces[0]
+            for (x, y, w, h) in faces:
+                face = Face(x, y, w, h)
+                break
 
-            cv2.rectangle(screen, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), (255, 255, 0), 2)
+            try:
 
-            cv2.imshow('img', screen)
+                x_note = translate_x_to_midi(face.x)
+                y_note = translate_y_to_midi(face.y)
+
+                vcv_midi_device.send_midi_velocity_note_to_channel(x_note, channel=0)
+                print(f'SENT VALUE {x_note} TO CHANNEL 0 (X AXIS)')
+                vcv_midi_device.send_midi_velocity_note_to_channel(y_note, channel=1)
+                print(f'SENT VALUE {y_note} TO CHANNEL 1 (Y AXIS)')
+
+            except:
+                print("Something went wrong sending midi to x,y channels")
+
+            if old_face.x != 0:
+                diff_val = abs(face.x - old_face.x)
+
+                try:
+                    diff_note = translate_diff_to_midi(diff_val ** 1.2)
+
+                    vcv_midi_device.send_midi_velocity_note_to_channel(int(diff_note), channel=2)
+                    print(f'SENT VALUE {diff_note} TO CHANNEL 2 (SPEED)')
+                except:
+                    print('Something went wrong when parsing movement rate')
+
+
+
+            old_face = face
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
 
-            send_midi_note(x_coordinate_to_midi(face[0]))
-
-
             # sleep(3)
 
-
             # turn_off_all_notes()
-
 
 main()
